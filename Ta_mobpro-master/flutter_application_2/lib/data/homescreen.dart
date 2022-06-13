036ed482412409e80baf9fdaec93e6d01d1a6d81
feriirawan023 <feri54381@gmail.com>
2022-06-13 19:49:05 +0700

    first commit

---
+++ Ta_mobpro-master/flutter_application_2/lib/data/homescreen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/data/data_rental.dart';
+import 'package:flutter_application_2/data/rental.dart';
+import 'package:flutter_application_2/data/profile.dart';
+
+// ignore: camel_case_types
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person, color: Colors.white),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Profile(),
+                ),
+              );
+            },
+          ),
+        ],
+        backgroundColor: const Color.fromRGBO(30, 107, 135, 100),
+        title: const Text('Daftar Mobil Rental'),
+      ),
+      body: ListView.builder(
+          itemCount: dataRental.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailScreen(
+                              rentals: dataRental[index],
+                            )));
+              },
+              child: Card(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ClipRRect(
+                        child: Image.asset(
+                          dataRental[index].image,
+                          width: 150,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(dataRental[index].name,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Color.fromRGBO(30, 107, 135, 100))),
+                        Text('Biaya Sewa : ${dataRental[index].sewa}'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
---
+++ Ta_mobpro-master/flutter_application_2/lib/data/profile.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const Profile());
+}
+
+class Profile extends StatefulWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(30, 107, 135, 100),
+          elevation: 0.0,
+          centerTitle: true,
+          title: const Text('Profile',
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+              )),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(30.0),
+          color: const Color.fromARGB(255, 255, 255, 255),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    const CircleAvatar(
+                      backgroundImage: AssetImage('assets/profil.png'),
+                      radius: 60,
+                    ),
+                    const SizedBox(height: 20.0),
+                    Text('Account',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            color: const Color.fromARGB(255, 43, 42, 42),
+                            fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 5.0),
+                    Text("Sewa Anda",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: const Color.fromARGB(255, 172, 169, 169),
+                        )),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+                  decoration: BoxDecoration(
+                      color: const Color.fromRGBO(30, 107, 135, 100),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                              bottom:
+                                  BorderSide(color: Colors.white, width: 1)),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Avanza",
+                                style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold)),
+                            Row(
+                              children: [
+                                Text("2 Bulan",
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.white,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold)),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
